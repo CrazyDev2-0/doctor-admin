a1e06a872ac1c629d43747d6ebcae951daa0cdad
Tanmoy Sarkar <ts741127@gmail.com>
2022-10-25 15:58:56 +0530

    alertbar isseu fixed

--- src/components/alertbar.jsx
+++ src/components/alertbar.jsx
@@ -1,66 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { faClose } from '@fortawesome/free-solid-svg-icons'
+import React, { useRef, useEffect } from "react";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { IconButton } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import AlertBarElement from "./alertbar_element";
 
-import firebaseapp from '../firebase'
+import firebaseapp from "../firebase";
 import { getMessaging, onMessage } from "firebase/messaging";
 import axios from "axios";
-    
 
-const BASEURL = 'https://42ca-103-211-134-133.in.ngrok.io';
-const AlertBar = ()=>{
+const BASEURL = "https://42ca-103-211-134-133.in.ngrok.io";
+const AlertBar = () => {
+  const notif = useRef([]);
 
-    const [notif, setNotif] = useState([]);
+  const [_, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  const getOldAlerts = async () => {
+    const data = await axios({
+      method: "get",
+      url: `${BASEURL}/doctor/alert`,
+      headers: {
+        authorization: localStorage.getItem("token"),
+      },
+    });
+    console.log(data.data.payload);
+    let tmp = data.data.payload;
+    notif.current= tmp;
+    forceUpdate();
+  };
+  useEffect(() => {
+    getOldAlerts();
+    const messaging = getMessaging(firebaseapp);
 
-    const [_, updateState] = React.useState();
-    const forceUpdate = React.useCallback(() => updateState({}), []);
-    const getOldAlerts = async () => {
-        const data = await axios({
-            method: 'get',
-            url: `${BASEURL}/doctor/alert`,
-            headers: { 
-                'authorization': localStorage.getItem('token')
-              }
-        });
-        console.log(data.data.payload);
-        let tmp =  data.data.payload.reverse();
-        setNotif(tmp);        
-    } 
-    useEffect(() => {
-        getOldAlerts();
-        const messaging = getMessaging(firebaseapp);
+    onMessage(messaging, function (payload) {
+      let tmp = notif.current;
+      const res = JSON.parse(payload.data.payload);
+      notif.current = [res, ...tmp];
+      forceUpdate();
+    });
 
-        onMessage(messaging, function (payload) {
-        console.log('Message received. ', payload);
-        let tmp = notif;
-        const res = JSON.parse(payload.data.payload);
-        // tmp.splice( 0, 0, res );
-        setNotif( [res, ...tmp] );
-        forceUpdate();
-        // setNotif([...notif, payload]);
-        console.log(res);
-        });
-    }, []);
+  }, []);
 
-    console.log("NOTIF", notif);
-
-    return(
+  return (
     <>
-    <div id="alertBar" className="alertBarContainer">
-        <div style={{float: "right"}}><IconButton aria-label='' onClick={()=>document.closeAlertBar()} icon={<FontAwesomeIcon icon={faClose} fontSize={20} />} /></div>
+      <div id="alertBar" className="alertBarContainer">
+        <div style={{ float: "right" }}>
+          <IconButton
+            aria-label=""
+            onClick={() => document.closeAlertBar()}
+            icon={<FontAwesomeIcon icon={faClose} fontSize={20} />}
+          />
+        </div>
         <div className="systemDetails">
-            <p>All Alerts</p>
+          <p>All Alerts</p>
         </div>
         <div className="divider"></div>
         <div className="alertBarContent">
-            {
-                notif.map( elem => <AlertBarElement alrt={ elem }/>)
-            }
+          {notif.current.map((elem) => (
+            <AlertBarElement alrt={elem} />
+          ))}
         </div>
-    </div>
-    </>);
-}
+      </div>
+    </>
+  );
+};
 
 export default AlertBar;
